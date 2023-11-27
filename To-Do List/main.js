5e40639f4f417bbd653a223580b7b1529c3f81d5
Iadivin <aimedifi003@gmail.com>
2023-11-27 09:28:56 +0200

    Project initialising

---
+++ To-Do List/main.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("additem");
+        const ul = document.getElementById("tasklist");
+        const input = document.getElementById("text");
+        
+        
+        const check = (box, cla) => {
+            if(document.getElementById(box).checked) {
+                document.getElementById(box).setAttribute('checked','checked');
+                document.getElementById(cla).setAttribute("class","change");
+            }
+        };
+
+        const remove = (cla) => {
+            console.log(ul.removeChild(document.getElementById(cla)));
+            //ul.children;
+        }
+
+        button.addEventListener('click', () =>{
+            ul.innerHTML += `<li id="id${ul.children.length}">
+                ${input.value} 
+                <input type="checkbox" id ="check${ul.children.length}" onclick ="check('check${ul.children.length}', 'id${ul.children.length}')"> 
+                <button onclick ="remove('id${ul.children.length}')">Remove</button>
+                </li>`;
+        });
