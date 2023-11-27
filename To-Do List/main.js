ba029914aa45f39720861b51d8bba81638b9820d
Iadivin <aimedifi003@gmail.com>
2023-11-27 13:04:09 +0200

    Element positioning

--- To-Do List/main.js
+++ To-Do List/main.js
@@ -3,12 +3,16 @@ const button = document.getElementById("additem");
         const input = document.getElementById("text");
         
         
-        const check = (box, cla) => {
+        const check = (box, id, cla) => {
             if(document.getElementById(box).checked) {
                 document.getElementById(box).setAttribute('checked','checked');
-                document.getElementById(cla).setAttribute("class","change");
+
+                document.getElementById(cla).setAttribute("class","textChange");
+
+                document.getElementById(id).setAttribute("class","change");
             } else {
                 document.getElementById(box).removeAttribute('checked')
+                document.getElementById(id).removeAttribute("class");
                 document.getElementById(cla).removeAttribute("class");
             }
         };
@@ -19,8 +23,10 @@ const button = document.getElementById("additem");
         }
 
         button.addEventListener('click', () =>{
-            ul.innerHTML += `<li id="id${ul.children.length}"><span>${input.value} </span>
-                <input type="checkbox" id ="check${ul.children.length}" onclick ="check('check${ul.children.length}', 'id${ul.children.length}')"> 
+            ul.innerHTML += `<li id="id${ul.children.length}">
+            <div><span id="class${ul.children.length}">${input.value} </span></div>
+            
+                <input type="checkbox" id ="check${ul.children.length}" onclick ="check('check${ul.children.length}', 'id${ul.children.length}', 'class${ul.children.length}')"> 
                 <button onclick ="remove('id${ul.children.length}')">Remove</button>
                 </li>`;
         });
