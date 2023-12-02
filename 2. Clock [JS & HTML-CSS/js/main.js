6a8bb67619e9946dc382cb20958451042205332f
Iadivin <aimedifi003@gmail.com>
2023-12-02 08:43:17 +0200

    hands movements

--- 2. Clock [JS & HTML-CSS/js/main.js
+++ 2. Clock [JS & HTML-CSS/js/main.js
@@ -2,5 +2,7 @@ const sec = document.getElementById('sec');
 
 setInterval(()=>{
     const date = new Date();
-    //sec.style.transform = `rotate(${((date.getSeconds()+30)/60 * 360)}deg)`;
+    sec.style.transform = `rotate(${(date.getSeconds()/60 * 360)-90}deg)`;
+    min.style.transform = `rotate(${(date.getMinutes()/60 * 360)-90}deg)`;
+    hour.style.transform = `rotate(${(date.getHours()/12 *(360))-90}deg)`;
 },1000)
