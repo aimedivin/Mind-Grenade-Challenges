e9f257ecb9d17ad2bc0d59f494fe1efd1a55ffe8
Iadivin <aimedifi003@gmail.com>
2023-12-02 10:43:58 +0200

    Digital hour retouch

--- 2. Clock [JS & HTML-CSS/js/main.js
+++ 2. Clock [JS & HTML-CSS/js/main.js
@@ -1,8 +1,19 @@
-const sec = document.getElementById('sec');
+const secNum = document.getElementsByClassName('secNum');
+const minNum = document.getElementsByClassName('minNum');
+const hourNum = document.getElementsByClassName('hourNum');
 
 setInterval(()=>{
     const date = new Date();
+    let sec_ = date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds();
+    let min_ = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes();
+    let hour_ = date.getHours() > 9 ? date.getHours() : '0' + date.getHours();
+
     sec.style.transform = `rotate(${(date.getSeconds()/60 * 360)-90}deg)`;
     min.style.transform = `rotate(${(date.getMinutes()/60 * 360)-90}deg)`;
     hour.style.transform = `rotate(${(date.getHours()/12 *(360))-90}deg)`;
-},1000)
+
+    //DIGITAL CLOCK
+    secNum[0].innerHTML = sec_
+    minNum[0].innerHTML = min_;
+    hourNum[0].innerHTML = hour_;
+},1000);
