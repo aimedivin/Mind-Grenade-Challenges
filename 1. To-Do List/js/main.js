e0a56f19516e7a21f917a24fd3ec562075714285
Iadivin <aimedifi003@gmail.com>
2024-01-21 11:02:58 +0200

    code retouch

--- 1. To-Do List/js/main.js
+++ 1. To-Do List/js/main.js
@@ -1,4 +1,4 @@
-const date = new Date()
+const date = new Date();
 const button = document.getElementById("additem");
 const ul = document.getElementById("tasklist");
 const input = document.getElementById("text");
@@ -28,7 +28,7 @@ const input = document.getElementById("text");
             <div>
             <span id="class${ul.children.length}">${input.value}
             </span>
-            <span class="date">${date.getFullYear()}/${date.getMonth()}/${date.getDay()}</span>
+            <span class="date">${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}</span>
             </div>
             <input type="checkbox" id ="check${ul.children.length}" onclick ="check('check${ul.children.length}', 'id${ul.children.length}', 'class${ul.children.length}')">
             <button onclick ="remove('id${ul.children.length}')">Remove</button>
